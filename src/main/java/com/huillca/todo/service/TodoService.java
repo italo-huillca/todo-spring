a44a00ec56f40bd5842a5e12abffc1cc7a23de0c
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-08 14:03:07 -0500

    feat: añadiendo servicio todoservice

--- src/main/java/com/huillca/todo/service/TodoService.java
+++ src/main/java/com/huillca/todo/service/TodoService.java
@@ -1,5 +1,22 @@
 package com.huillca.todo.service;
 
+import com.huillca.todo.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TodoService {
-    
+    private List<Todo> todos = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<Todo> getAllTodos() {
+        return todos;
+    }
+
+    public void addTodo(Todo todo) {
+        todo.setId(nextId++);
+        todos.add(todo);
+    }
 }
