4d82d87c1e593f0531b704d1a4942a527a191ce8
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-08 14:03:48 -0500

    feat: añadiendo controlador del to-do

--- src/main/java/com/huillca/todo/controller/TodoController.java
+++ src/main/java/com/huillca/todo/controller/TodoController.java
@@ -1,5 +1,38 @@
 package com.huillca.todo.controller;
 
+import com.huillca.todo.model.Todo;
+import com.huillca.todo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class TodoController {
-    
+
+    @Autowired
+    private TodoService todoService;
+
+    // Mostrar la lista de tareas
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("todos", todoService.getAllTodos());
+        return "index";
+    }
+
+    // Mostrar formulario para agregar tarea
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "add-todo";
+    }
+
+    // Procesar el formulario para agregar tarea
+    @PostMapping("/add")
+    public String addTodo(@ModelAttribute Todo todo) {
+        todoService.addTodo(todo);
+        return "redirect:/";
+    }
 }
