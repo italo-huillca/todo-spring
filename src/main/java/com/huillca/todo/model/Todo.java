098306b65ebcd5657a6259d4d10c90233c5af5ff
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-08 14:02:30 -0500

    feat: añadiendo modelo todo

--- src/main/java/com/huillca/todo/model/Todo.java
+++ src/main/java/com/huillca/todo/model/Todo.java
@@ -1,5 +1,42 @@
 package com.huillca.todo.model;
 
 public class Todo {
-    
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    // Constructor vacío
+    public Todo() {}
+
+    // Constructor con parámetros
+    public Todo(Long id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
