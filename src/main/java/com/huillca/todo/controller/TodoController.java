7ba623daf84720090d10cc3cc5d3736d1a872cad
Italo H <yhon.huillca@tecsup.edu.pe>
2025-05-08 14:38:07 -0500

    feat: añadir selección de usuario al agregar tarea

--- src/main/java/com/huillca/todo/controller/TodoController.java
+++ src/main/java/com/huillca/todo/controller/TodoController.java
@@ -1,13 +1,13 @@
 package com.huillca.todo.controller;
 
 import com.huillca.todo.model.Todo;
+import com.huillca.todo.model.User;
 import com.huillca.todo.service.TodoService;
+import com.huillca.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
@@ -15,23 +15,26 @@ public class TodoController {
     @Autowired
     private TodoService todoService;
 
-    // Mostrar la lista de tareas
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("todos", todoService.getAllTodos());
         return "index";
     }
 
-    // Mostrar formulario para agregar tarea
     @GetMapping("/add")
     public String showAddForm(Model model) {
         model.addAttribute("todo", new Todo());
+        model.addAttribute("users", userService.getAllUsers());
         return "add-todo";
     }
 
-    // Procesar el formulario para agregar tarea
     @PostMapping("/add")
-    public String addTodo(@ModelAttribute Todo todo) {
+    public String addTodo(@ModelAttribute Todo todo, @RequestParam("user.id") Long userId) {
+        User user = userService.getUserById(userId);
+        todo.setUser(user);
         todoService.addTodo(todo);
         return "redirect:/";
     }
